fdbcf6009e01af8630728b2a98be27ac0b0afbaa
kdnakt <a.kid.1985@gmail.com>
2021-05-10 22:14:32 +0900

    comment out config

--- myapp/build.gradle.kts
+++ myapp/build.gradle.kts
@@ -48,11 +48,11 @@ detekt {
 
     // The directories where detekt looks for source files.
     // Defaults to `files("src/main/java", "src/main/kotlin")`.
-    input = files("src/main/java", "src/main/kotlin")
+    input = files("src/main/kotlin")
 
     // Builds the AST in parallel. Rules are always executed in parallel.
     // Can lead to speedups in larger projects. `false` by default.
-    parallel = false
+    // parallel = false
 
     // Define the detekt configuration(s) you want to use.
     // Defaults to the default detekt configuration.
@@ -62,10 +62,10 @@ detekt {
     buildUponDefaultConfig = true
 
     // Turns on all the rules. `false` by default.
-    allRules = false
+    allRules = true
 
     // Specifying a baseline file. All findings stored in this file in subsequent runs of detekt.
-    baseline = file("path/to/baseline.xml")
+    // baseline = file("path/to/baseline.xml")
 
     // Disables all default detekt rulesets and will only run detekt with custom rules
     // defined in plugins passed in with `detektPlugins` configuration. `false` by default.
@@ -79,13 +79,13 @@ detekt {
     ignoreFailures = false
 
     // Android: Don't create tasks for the specified build types (e.g. "release")
-    ignoredBuildTypes = listOf("release")
+    // ignoredBuildTypes = listOf("release")
 
     // Android: Don't create tasks for the specified build flavor (e.g. "production")
-    ignoredFlavors = listOf("production")
+    // ignoredFlavors = listOf("production")
 
     // Android: Don't create tasks for the specified build variants (e.g. "productionRelease")
-    ignoredVariants = listOf("productionRelease")
+    // ignoredVariants = listOf("productionRelease")
 
     // Specify the base path for file paths in the formatted reports.
     // If not set, all file paths reported will be absolute file path.
