f1199b79b0e63237a65bdbb2ebe26907c5ce40de
kdnakt <a.kid.1985@gmail.com>
2021-05-10 22:12:07 +0900

    add too many functions check

--- myapp/build.gradle.kts
+++ myapp/build.gradle.kts
@@ -9,6 +9,8 @@ plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin on the JVM.
     id("org.jetbrains.kotlin.jvm").version("1.3.21")
 
+    id("io.gitlab.arturbosch.detekt").version("1.16.0")
+
     // Apply the application plugin to add support for building a CLI application.
     application
 }
@@ -17,6 +19,9 @@ repositories {
     // Use jcenter for resolving your dependencies.
     // You can declare any Maven/Ivy/file repository here.
     jcenter()
+
+    // For detekt
+    mavenCentral()
 }
 
 dependencies {
@@ -33,4 +38,84 @@ dependencies {
 application {
     // Define the main class for the application.
     mainClassName = "com.kdnakt.myapp.AppKt"
+}
+
+
+detekt {
+    // Version of Detekt that will be used. When unspecified the latest detekt
+    // version found will be used. Override to stay on the same version.
+    toolVersion = "1.16.0"
+
+    // The directories where detekt looks for source files.
+    // Defaults to `files("src/main/java", "src/main/kotlin")`.
+    input = files("src/main/java", "src/main/kotlin")
+
+    // Builds the AST in parallel. Rules are always executed in parallel.
+    // Can lead to speedups in larger projects. `false` by default.
+    parallel = false
+
+    // Define the detekt configuration(s) you want to use.
+    // Defaults to the default detekt configuration.
+    config = files("detekt.config.yml")
+
+    // Applies the config files on top of detekt's default config file. `false` by default.
+    buildUponDefaultConfig = true
+
+    // Turns on all the rules. `false` by default.
+    allRules = false
+
+    // Specifying a baseline file. All findings stored in this file in subsequent runs of detekt.
+    baseline = file("path/to/baseline.xml")
+
+    // Disables all default detekt rulesets and will only run detekt with custom rules
+    // defined in plugins passed in with `detektPlugins` configuration. `false` by default.
+    disableDefaultRuleSets = false
+
+    // Adds debug output during task execution. `false` by default.
+    debug = false
+
+    // If set to `true` the build does not fail when the
+    // maxIssues count was reached. Defaults to `false`.
+    ignoreFailures = false
+
+    // Android: Don't create tasks for the specified build types (e.g. "release")
+    ignoredBuildTypes = listOf("release")
+
+    // Android: Don't create tasks for the specified build flavor (e.g. "production")
+    ignoredFlavors = listOf("production")
+
+    // Android: Don't create tasks for the specified build variants (e.g. "productionRelease")
+    ignoredVariants = listOf("productionRelease")
+
+    // Specify the base path for file paths in the formatted reports.
+    // If not set, all file paths reported will be absolute file path.
+    // basePath = projectDir
+
+    reports {
+        // Enable/Disable XML report (default: true)
+        xml {
+            enabled = false
+            destination = file("build/reports/detekt.xml")
+        }
+        // Enable/Disable HTML report (default: true)
+        html {
+            enabled = true
+            destination = file("build/reports/detekt.html")
+        }
+        // Enable/Disable TXT report (default: true)
+        txt {
+            enabled = true
+            destination = file("build/reports/detekt.txt")
+        }
+        // Enable/Disable SARIF report (default: false)
+        sarif {
+            enabled = false
+            destination = file("build/reports/detekt.sarif")
+        }
+        // custom {
+            // The simple class name of your custom report.
+        //    reportId = "CustomJsonReport"
+        //    destination = file("build/reports/detekt.json")
+        // }
+    }
 }
--- myapp/src/main/kotlin/com/kdnakt/myapp/App.kt
+++ myapp/src/main/kotlin/com/kdnakt/myapp/App.kt
@@ -1,3 +1,9 @@
 package com.kdnakt.myapp
 
 fun main() = println("Hello world")
+
+fun myFun() = "myFun"
+
+fun myFun2() = "myFun2"
+
+fun myFun3() = "myFun3"
