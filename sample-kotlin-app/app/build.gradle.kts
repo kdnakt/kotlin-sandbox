8f771414e763a12472abc153ef69b20b5760cc3d
kdnakt <a.kid.1985@gmail.com>
2021-09-02 23:55:26 +0900

    apply jacoco plugin

--- sample-kotlin-app/app/build.gradle.kts
+++ sample-kotlin-app/app/build.gradle.kts
@@ -10,6 +10,8 @@ plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.4.31"
 
+    id("jacoco")
+
     // Apply the application plugin to add support for building a CLI application in Java.
     application
 }
